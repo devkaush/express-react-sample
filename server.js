ac1b010787da270971da6617969c8bc042137e4a
devkaush <devkaush123@gmail.com>
2020-08-13 10:30:08 +0530

    few changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+//pp.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', function(req, res,) {
     res.send('API is working properly');
